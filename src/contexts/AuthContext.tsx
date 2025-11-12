9979ebd128b1c39fc0fe81607ecf078c42d02ae4
Igor Cavalcanti <igormbcavalcanti@gmail.com>
2025-11-12 12:02:05 -0300

    fix: corrige caminho de importação em AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../services/firebase";
+import { auth } from "../services/firebaseConfig";
 
 const login = async (matricula:string, senha:string) => {
   try {
